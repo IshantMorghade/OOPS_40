37d7ef9f35c25d27595c3cbf36eee50ceb1a1f37
Ishant Morghade <ishantmorghade1@gmail.com>
2024-03-15 17:45:32 +0530

    Practical 6 Executed Successfully.
    Program On: Package.

---
+++ Practical_6/Practical_6.java
@@ -0,0 +1,14 @@
+import learnjava.First;
+import Ishant.Ishant;
+
+public class Practical_6 
+{
+    public static void main(String[] args) 
+{
+        First firstObject = new First();
+        firstObject.display();
+
+        Ishant object2 = new Ishant();
+        object2.display();
+    }
+}
